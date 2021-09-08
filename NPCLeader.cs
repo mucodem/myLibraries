ad7e5f3d9ab74b7a00ab97b828563f9a4ad0eb78
Mücahit DEMİRCİ <65566000+mucodem@users.noreply.github.com>
2021-09-08 16:46:55 +0300

    First

---
+++ NPCLeader.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCLeader : MonoBehaviour
+{
+    public enum LeaderMoveType
+    {
+        Stroll, RunAwayOrChase
+    }
+
+    [Header("Needed values")]
+    [SerializeField] Transform player;
+    [SerializeField] float multiplier;
+    [SerializeField] float range = 30f;
+
+    [Header("References")]
+    Spawner spawner;
+    NavMeshAgent agent;
+    LeaderMoveType moveType;
+    Leader leader;
+    [SerializeField] UIManager uIManager;
+
+    void Start()
+    {
+        spawner = FindObjectOfType<Spawner>();
+        agent = GetComponent<NavMeshAgent>();
+        leader = GetComponent<Leader>();
+        StartCoroutine(StrollAround());
+    }
+
+    void Update()
+    {
+        switch (moveType)
+        {
+            case LeaderMoveType.Stroll:
+                CheckAndReStroll();
+                break;
+            case LeaderMoveType.RunAwayOrChase:
+                Chechker();
+                break;
+        }
+    }
+
+    IEnumerator StrollAround() // set random target to go
+    {
+        yield return new WaitForEndOfFrame();
+        agent.SetDestination(spawner.spawnPoint[Random.Range(0, spawner.spawnPoint.Length)].position); // set a random location to go for agent
+    }
+
+    void CheckAndReStroll() // if reached to target , set a new target
+    {
+        if (agent.remainingDistance <= 1f)
+        {
+            //Debug.Log("REMAINING DISTANCE IS LESS THEN 1");
+            agent.SetDestination(spawner.spawnPoint[Random.Range(0, spawner.spawnPoint.Length)].position); // set a random location to go for agent
+        }
+    }
+
+    void Chechker()
+    {
+        Vector3 runTo = transform.position + ((transform.position - player.position) * multiplier);
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance < range) 
+        {
+            if(leader.follower.Count > player.GetComponent<Leader>().follower.Count)
+            {
+                agent.SetDestination(-runTo); // chase
+            }
+            else
+            {
+                agent.SetDestination(runTo); // run away
+            }
+        }     
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    [SerializeField] FloatingJoystick joystick;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float forwardSpeed;
+
+    [Header("Player Components")]
+    Rigidbody rb;
+
+    public float joystickH;
+    public float joystickV;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        joystickH = joystick.Horizontal;
+        joystickV = joystick.Vertical;
+
+        rb.velocity = forwardSpeed * Time.deltaTime * transform.forward;
+
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            Rotate(new Vector3(joystick.Horizontal, 0, joystick.Vertical));
+        }
+           
+    }
+
+    void Rotate(Vector3 direction)
+    {
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, direction, rotationSpeed * Time.deltaTime, .1f);
+        Quaternion _newRotation = Quaternion.LookRotation(desiredForward);
+        transform.rotation = _newRotation;
+    }
+
+    //void RotateMethod2()
+    //{
+    //    Vector3 lookAtPos = leader.forward;
+    //    Quaternion newRotation = Quaternion.LookRotation(lookAtPos, transform.up);
+    //    leader.transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 8);
+    //}
+}
